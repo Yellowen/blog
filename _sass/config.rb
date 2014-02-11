3bdecf65d6074a555c474f21a05cd1a16fe1cf7f
Sameer Rahmani <lxsameer@gnu.org>
2014-02-12 00:20:17 +0330

    first working configuration

--- _sass/config.rb
+++ _sass/config.rb
@@ -23,6 +23,6 @@ require 'semantic-ui-sass'
 http_path = "/"
 sass_dir = "./"
 css_dir = "../css"
-images_dir = "../img"
-javascripts_dir = "../js"
+images_dir = "../images"
+javascripts_dir = "../javascripts"
 relative_assets = true
